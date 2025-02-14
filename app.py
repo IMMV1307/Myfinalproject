8fa5799d55f7b2db0b99e203f65aa2460e161617
IMMV1307 <ingridvillasis@gmail.com>
2025-02-13 20:19:04 -0600

    Accept Current Change

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') 
+
+st.header("Dashboard de Análisis de Vehículos Usados")
+
 hist_button = st.button('Construir histograma') 
+
+if st.checkbox("Visualizar muestra de datos"):
+    st.write(car_data.head())
         
-if hist_button: 
-    st.write("vehicles_us.csv")
+if hist_button ("KM recorridos"): 
+    st.write(car_data.head())
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 hist_button_2 = st.button('Construir grafico de dispersión') 
         
-if hist_button_2: 
-    st.write("vehicles_us.csv")
-    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+if hist_button_2 ("Precio vs KM recorridos"): 
+    st.write(car_data.head())
+    fig = px.scatter(car_data, x="odometer", y="price", title= "Precio del vehículo en relación a los KM recorridos y año" ) # crear un gráfico de dispersión
     fig.show()
