cbca2c27af1b05382b08056f461754f9ad659c83
IMMV1307 <ingridvillasis@gmail.com>
2025-02-13 19:48:57 -0600

    Accept current change

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma') 
         
 if hist_button: 
-    st.write("C:\\Users\\Usuario\\OneDrive\\Documentos\\Myfinalproject\\vehicles_us.csv")
+    st.write("vehicles_us.csv")
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 hist_button_2 = st.button('Construir grafico de dispersión') 
         
 if hist_button_2: 
-    st.write("C:\\Users\\Usuario\\OneDrive\\Documentos\\Myfinalproject\\vehicles_us.csv")
+    st.write("vehicles_us.csv")
     fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
     fig.show()
